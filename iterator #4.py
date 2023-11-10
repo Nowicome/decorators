7179ffccfc7360c444122989657f0ad26b2a25b5
Anton Shubin <rtl8187b@gmail.com>
2023-11-10 23:33:45 +0600

    Initial commit

---
+++ iterator #4.py
@@ -0,0 +1,36 @@
+import types
+from logger_1 import logger
+
+
+@logger
+def flat_generator(list_of_lists):
+    for i in list_of_lists:
+        if type(i) is list:
+            for j in flat_generator(i):
+                yield j
+        else:
+            yield i
+
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
